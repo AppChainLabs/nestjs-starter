e56c94cfab07368c639410dd189d0d9d0d5741a5
0xAppChainLabs <hello@appchain.cloud>
2022-03-30 23:08:17 +0700

    feat: enable jwt authentication

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,12 +9,15 @@ import {
   Query,
   UseGuards,
   Optional,
+  SetMetadata,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { RolesGuard } from '../auth/roles-guard.guard';
+import { JwtAuthGuard } from '../auth/jwt-guard.guard';
+import { UserRole } from './entities/user.entity';
 
 @ApiTags('user')
 @Controller('api/user')
@@ -23,11 +26,15 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @SetMetadata('roles', [UserRole.SystemAdmin])
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @SetMetadata('roles', [UserRole.SystemAdmin])
   findAll(
     @Query('searchQuery') searchQuery: string,
     @Query('limit') limit: number,
@@ -38,16 +45,22 @@ export class UserController {
   }
 
   @Get(':user_id')
+  @UseGuards(JwtAuthGuard)
+  @SetMetadata('roles', [UserRole.SystemAdmin])
   findOne(@Param('user_id') id: string) {
     return this.userService.findById(id);
   }
 
   @Patch(':user_id')
+  @UseGuards(JwtAuthGuard)
+  @SetMetadata('roles', [UserRole.SystemAdmin])
   update(@Param('user_id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':user_id')
+  @UseGuards(JwtAuthGuard)
+  @SetMetadata('roles', [UserRole.SystemAdmin])
   remove(@Param('user_id') id: string) {
     return this.userService.remove(id);
   }
