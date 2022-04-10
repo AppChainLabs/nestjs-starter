84721b1a9cab81540e11f3ecf771674f08716945
0xAppChainLabs <hello@appchain.cloud>
2022-04-10 15:58:53 +0700

    feat: clean up

--- src/providers/otp.ts
+++ src/providers/otp.ts
@@ -3,6 +3,12 @@ import { totp } from 'otplib';
 
 @Injectable()
 export class Otp {
+  constructor() {
+    totp.options = {
+      ...totp.options,
+      step: 60, // token expires in 60s
+    };
+  }
   generateToken(secret: string) {
     return totp.generate(secret);
   }
